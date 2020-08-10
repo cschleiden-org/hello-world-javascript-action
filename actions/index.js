5c2c848f595c32d7d9bab04112e1c94539f4c9de
Emily Lepert <elepert@olin.edu>
2020-08-10 16:29:04 -0700

    edit

--- actions/index.js
+++ actions/index.js
@@ -7,19 +7,23 @@ const existsAsync = promisify(exists);
 const writeFileAsync = promisify(writeFile);
 const statAsync = promisify(stat);
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  console.log(github.context.payload.pull_request.title);
-  await appendFileAsync("./Readme.md", "\n New line");
-  const statResult = await statAsync("./Readme.md");
-  setOutput("size", `${statResult.size}`);
-  // const time = (new Date()).toTimeString();
-  // core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+main().catch((error) => setFailed(error.message));
+
+async function main() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    console.log(github.context.payload.pull_request.title);
+    await appendFileAsync("./Readme.md", "\n New line");
+    const statResult = await statAsync("./Readme.md");
+    setOutput("size", `${statResult.size}`);
+    // const time = (new Date()).toTimeString();
+    // core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
