3b614307a5c3d9a0067b46dbf038b80c019b4ba8
Emily Lepert <elepert@olin.edu>
2020-08-10 16:24:34 -0700

    edit

--- actions/index.js
+++ actions/index.js
@@ -1,16 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+import { promisify } from "util";
+
+const appendFileAsync = promisify(appendFile);
+const existsAsync = promisify(exists);
+const writeFileAsync = promisify(writeFile);
+const statAsync = promisify(stat);
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   console.log(github.context.payload.pull_request.title);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  await appendFileAsync("./Readme.md", "\n New line");
+  const statResult = await statAsync("./Readme.md");
+  setOutput("size", `${statResult.size}`);
+  // const time = (new Date()).toTimeString();
+  // core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
