c39ad4b06bbe498b189c41bd11ed8cb7b28a7363
Emily Lepert <elepert@olin.edu>
2020-08-10 15:52:42 -0700

    edit

--- actions/index.js
+++ actions/index.js
@@ -5,7 +5,7 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
-  console.log(github.context.payload.pull_request.head.title);
+  console.log(github.context.payload.pull_request.title);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
