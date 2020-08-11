5cf6ee694ae7addb5ffd63273b10b7d187347d3f
Emily Lepert <elepert@olin.edu>
2020-08-11 10:19:23 -0700

    edit

--- actions/index.js
+++ actions/index.js
@@ -16,37 +16,37 @@ async function main() {
     const prName = github.context.payload.pull_request.title;
     const prLink = github.context.payload.pull_request.html_url;
     const prNum = github.context.payload.pull_request.number;
-    let prSplit = prName.split("(");
-    let changelogLine = "\n- ";
-    switch (prSplit[0]) {
-      case 'feat':
-        changelogLine += "[Feature]";
-        break;
-      case 'patch':
-        changelogLine += "[Patch]";
-        break;
-      case 'real':
-        changelogLine += "[Release]";
-        break;
-      default:
-        return;
-    }
-    prSplit = prSplit[1].split(")");
-    changelogLine += `${prSplit[1]} in **${prSplit[0]}** ([#${prNum}](${prLink}))`;
-    console.log(changelogLine);
+    // let prSplit = prName.split("(");
+    // let changelogLine = "\n- ";
+    // switch (prSplit[0]) {
+    //   case 'feat':
+    //     changelogLine += "[Feature]";
+    //     break;
+    //   case 'patch':
+    //     changelogLine += "[Patch]";
+    //     break;
+    //   case 'real':
+    //     changelogLine += "[Release]";
+    //     break;
+    //   default:
+    //     return;
+    // }
+    // prSplit = prSplit[1].split(")");
+    // changelogLine += `${prSplit[1]} in **${prSplit[0]}** ([#${prNum}](${prLink}))`;
+    // console.log(changelogLine);
 
-    const path = "./Readme.md";
-    const fileContents = readFileSync(path,'utf8');
-    const splitFile = fileContents.split("## Unreleased\n");
-    let finalContents = `${splitFile[0]}## Unreleased\n`;
-    finalContents += changelogLine;
-    // console.log(splitFile[1][0:10])
-    if (splitFile[1][2] == "#") {
-      finalContents += "\n";
-    }
-    finalContents += splitFile[1];
+    // const path = "./Readme.md";
+    // const fileContents = readFileSync(path,'utf8');
+    // const splitFile = fileContents.split("## Unreleased\n");
+    // let finalContents = `${splitFile[0]}## Unreleased\n`;
+    // finalContents += changelogLine;
+    // // console.log(splitFile[1][0:10])
+    // if (splitFile[1][2] == "#") {
+    //   finalContents += "\n";
+    // }
+    // finalContents += splitFile[1];
 
-    await writeFileAsync(path, finalContents);
+    // await writeFileAsync(path, finalContents);
     // const statResult = await statAsync("./Readme.md");
     // setOutput("size", `${statResult.size}`);
     const time = (new Date()).toTimeString();
