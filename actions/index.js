32cb5a42f21466efc333e5b9955f7313a594065d
Emily Lepert <elepert@olin.edu>
2020-08-12 17:59:14 -0700

    edit

--- actions/index.js
+++ actions/index.js
@@ -69,7 +69,7 @@ async function main() {
 
       // write to file
       await writeFileAsync(path, finalContents);
-      commentMessage= ":tada: Updated the Unreleased section of the Changelog with '" + changelogLine + "'"
+      commentMessage= ":tada:  Updated the Unreleased section of the Changelog with ```\n" + changelogLine + "\n```"
     }
 
     // start process for writing PR comment
